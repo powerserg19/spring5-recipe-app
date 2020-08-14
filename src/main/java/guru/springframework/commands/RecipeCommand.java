91bd1acbd769153120d8854390a7ec9865c4861e
sergio <powerserg19@hotmail.com>
2020-08-14 08:53:46 -0400

    end of 199

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -26,6 +26,6 @@ public class RecipeCommand {
     private String directions;
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulty difficulty;
-    private NotesCommand notes = new NotesCommand();
+    private NotesCommand notes;
     private Set<CategoryCommand> categories = new HashSet<>();
 }
